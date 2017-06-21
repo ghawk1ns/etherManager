31f65b7c25ab29a0f17470330aa06fe9c9532ef6
GHawk1ns <guy.hawkins@utexas.edu>
2017-06-21 07:40:09 +0000

    modified run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 case $1 in
     start)
        echo $$ > /var/run/etherManager.pid;
-       exec /opt/EthosRigManager/./gradlew run
+       exec /opt/etherManager/./gradlew run
        ;;
     stop)  
       kill `cat /var/run/etherManager.pid` ;;
